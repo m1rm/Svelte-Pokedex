95729a3a6be137edadf176199b54349ad38cf7a8
shushiro <miriam.uni@outlook.de>
2022-02-13 23:24:36 +0100

    add props for Pokecard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	export let name;
-	// import Pokecard from './components/Pokecard.svelte';
+	import Pokecard from './components/Pokecard.svelte';
 	import pokemon from './assets/pokeData.json';
 	console.log(pokemon);
 </script>
@@ -9,13 +9,17 @@
 	<h1>Hello {name}!</h1>
 	<div class="flex-container">
 		{#each pokemon as monster}
-			<div>{monster.name}</div>
+			<Pokecard
+					  name="{monster.name}"
+					  pokedexIndex="{monster.pokedexIndex}"
+					  type="{monster.type}"
+					  portrait="{monster.portrait}"
+			></Pokecard>
 		{/each}
 	</div>
 </main>
 
 <style>
-
 	h1 {
 		font-size:48px;
 		text-align:center !important;
--- src/components/Pokecard.svelte
+++ src/components/Pokecard.svelte
@@ -1,69 +1,73 @@
-<!--<div id="">-->
-<!--    <div class='pokecard-flex-container'>-->
-<!--        <span class='name-number'> {{ name }} </span>-->
-<!--        <span class='name-number'>#{{ pokedexIndex }}</span>-->
-<!--        <div class='flex-break'></div>-->
-<!--        <img src='{{ portrait }}' alt='{{ name }}-image'>-->
-<!--    </div>-->
-<!--    <div class='pokecard-flex-container'>-->
-<!--        <span class='type-badge'>{{ type[0] }}</span>-->
-<!--    </div>-->
-<!--</div>-->
+<script>
+   export let name = "undefined";
+   export let pokedexIndex = "000";
+   export let portrait = "https://via.placeholder.com/150";
+   export let type = ["undefined"];
+</script>
 
+<div class="{ type[0] }">
+    <div class='pokecard-flex-container'>
+        <span class='name-number'> { name }</span>
+        <span class='name-number'>#{ pokedexIndex }</span>
+        <div class='flex-break'></div>
+        <img src='{ portrait }' alt='{ name }-image'>
+    </div>
+    <div class='pokecard-flex-container'>
+        <span class='type-badge'>{ type[0] }</span>
+    </div>
+</div>
 
+<style>
+    .flex-item {
+        flex: 0 0 25%;
+        background-color: #808080;
+        height: 400px;
+        box-shadow: 0 0 6px #404040;
+        margin-top: 4rem;
+        border-radius: 5px;
+    }
 
-<!--<style>-->
-<!--    .flex-item {-->
-<!--        flex: 0 0 25%;-->
-<!--        background-color: #808080;-->
-<!--        height: 400px;-->
-<!--        box-shadow: 0 0 6px #404040;-->
-<!--        margin-top: 4rem;-->
-<!--        border-radius: 5px;-->
-<!--    }-->
+    .flex-break {
+        flex-basis: 100%;
+        height: 0;
+    }
 
-<!--    .flex-break {-->
-<!--        flex-basis: 100%;-->
-<!--        height: 0;-->
-<!--    }-->
+    .pokecard-flex-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
 
-<!--    .pokecard-flex-container {-->
-<!--        display: flex;-->
-<!--        flex-wrap: wrap;-->
-<!--        justify-content: space-around;-->
-<!--    }-->
+    .pokecard-flex-container .name-number {
+        margin-top: 2rem;
+    }
 
-<!--    .pokecard-flex-container .name-number {-->
-<!--        margin-top: 2rem;-->
-<!--    }-->
+    .pokecard-flex-container img {
+        margin-top: 1rem;
+        padding: 1rem;
+        height: 250px;
+        width: 250px;
+        box-shadow: 0 0 4px #404040;
+    }
 
-<!--    .pokecard-flex-container img {-->
-<!--        margin-top: 1rem;-->
-<!--        padding: 1rem;-->
-<!--        height: 250px;-->
-<!--        width: 250px;-->
-<!--        box-shadow: 0 0 4px #404040;-->
-<!--    }-->
-
-<!--    .type-badge {-->
-<!--        border-radius: 10px;-->
-<!--        box-shadow: 0 0 6px #404040;-->
-<!--        margin-top: 0.75rem;-->
-<!--        padding: 0 10px 0 10px;-->
-<!--    }-->
-
-<!--    .grass { background-color: #4FC1A6; opacity: 0.8; }-->
-<!--    .fire { background-color: #FA6555; opacity: 0.8;}-->
-<!--    .water { background-color: #429BED; opacity: 0.8; }-->
-<!--    .bug { background-color: #32CD32; opacity: 0.8; }-->
-<!--    .normal { background-color: #99AABB; opacity: 0.8; }-->
-<!--    .poison { background-color: #7C538C; opacity: 0.8; }-->
-<!--    .electric { background-color: #F6C747; opacity: 0.8; }-->
-<!--    .ground { background-color: #B1736C; opacity: 0.8; }-->
-<!--    .fighting { background-color: #99AABB; opacity: 0.8; }-->
-<!--    .psychic {background-color: #7C538C; opacity: 0.8; }-->
-<!--    .rock { background-color: #303943; opacity: 0.8; }-->
-<!--    .ghost { background-color: #7C538C; opacity: 0.8; }-->
-<!--    .ice { background-color: #429BED; opacity: 0.8; }-->
-<!--    .dragon { background-color: #B1736C; opacity: 0.8; }-->
-<!--</style>-->
+    .type-badge {
+        border-radius: 10px;
+        box-shadow: 0 0 6px #404040;
+        margin-top: 0.75rem;
+        padding: 0 10px 0 10px;
+    }
+    .grass { background-color: #4FC1A6; opacity: 0.8; }
+    .fire { background-color: #FA6555; opacity: 0.8;}
+    .water { background-color: #429BED; opacity: 0.8; }
+    .bug { background-color: #32CD32; opacity: 0.8; }
+    .normal { background-color: #99AABB; opacity: 0.8; }
+    .poison { background-color: #7C538C; opacity: 0.8; }
+    .electric { background-color: #F6C747; opacity: 0.8; }
+    .ground { background-color: #B1736C; opacity: 0.8; }
+    .fighting { background-color: #99AABB; opacity: 0.8; }
+    .psychic {background-color: #7C538C; opacity: 0.8; }
+    .rock { background-color: #303943; opacity: 0.8; }
+    .ghost { background-color: #7C538C; opacity: 0.8; }
+    .ice { background-color: #429BED; opacity: 0.8; }
+    .dragon { background-color: #B1736C; opacity: 0.8; }
+</style>
