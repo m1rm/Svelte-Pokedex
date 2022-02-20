c0d3904351054e6f154c4244936248ffabff6ea7
shushiro <miriam.uni@outlook.de>
2022-02-20 16:47:52 +0100

    move Pokedex to own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,8 @@
 <script>
-	export let name;
-	import Pokecard from './components/Pokecard.svelte';
-	import pokemon from './assets/pokeData.json';
-	console.log(pokemon);
+	import Pokedex from "./components/Pokedex.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<div class="flex-container">
-		{#each pokemon as monster}
-			<Pokecard
-					  name={monster.name}
-					  pokedexIndex={monster.pokedexIndex}
-					  type={monster.type}
-					  portrait={monster.img}
-			></Pokecard>
-		{/each}
-	</div>
+	<Pokedex></Pokedex>
+
 </main>
-
-<style>
-	h1 {
-		font-size:48px;
-		text-align:center !important;
-		display:inline-block !important;
-		width:100%;
-	}
-
-	.flex-container {
-		margin: 1rem 1rem 0 1rem;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-around;
-	}
-</style>
---
+++ src/components/Pokedex.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Pokecard from '../components/Pokecard.svelte';
+    import pokemon from '../assets/pokeData.json';
+    console.log(pokemon);
+</script>
+
+<div class="flex-container">
+    <h1>Svelte Pokédex</h1>
+    {#each pokemon as monster}
+        <Pokecard
+                name={monster.name}
+                pokedexIndex={monster.pokedexIndex}
+                type={monster.type}
+                portrait={monster.img}
+        ></Pokecard>
+    {/each}
+</div>
+
+<style>
+    h1 {
+        font-size:48px;
+        text-align:center !important;
+        display:inline-block !important;
+        width:100%;
+    }
+
+    .flex-container {
+        margin: 1rem 1rem 0 1rem;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+</style>
