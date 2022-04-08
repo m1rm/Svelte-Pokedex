50b8b815148df12448df6746c64f6796442b1757
shushiro <miriam.uni@outlook.de>
2022-04-08 23:09:53 +0200

    use Bing News API

--- src/components/News.svelte
+++ src/components/News.svelte
@@ -1,4 +1,23 @@
-<div class="flex-container">This is News</div>
+<div class="flex-container">
+
+    {#await data then value}
+        {#each data as resultObject}
+            <div class="news-card">
+                {#if resultObject.image}
+                    <img src="{resultObject.image.thumbnail.contentUrl}" alt="news teaser"/>
+                {:else}
+                    <img src="images/default-placeholder.png" alt="placholder" height="100px" width="100px"/>
+                {/if}
+                <div class="news-card-content">
+                    <span>{resultObject.name}</span>
+                    <span>{resultObject.description}</span>
+                </div>
+            </div>
+
+        {/each}
+    {/await}
+
+</div>
 
 <style>
     .flex-container {
@@ -7,4 +26,35 @@
         flex-wrap: wrap;
         justify-content: space-around;
     }
+
+    .news-card {
+        display: flex;
+        align-items: center;
+    }
+
+    .news-card-content {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
 </style>
+
+<script>
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-BingApis-SDK': process.env.BING_API_SDK,
+            'X-RapidAPI-Host': process.env.API_HOST,
+            'X-RapidAPI-Key': process.env.API_KEY
+        }
+    };
+
+    let data = [];
+
+    fetch('https://bing-news-search1.p.rapidapi.com/news/search?q=pokemon&safeSearch=Off&textFormat=Raw&freshness=Day', options)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response.value)
+            data = response.value;
+        })
+</script>
