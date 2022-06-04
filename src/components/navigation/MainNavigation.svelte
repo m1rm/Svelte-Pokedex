8863b506f50c1ebd01f93aeaeab4b163287a3276
shushiro <miriam.uni@outlook.de>
2022-06-04 18:15:56 +0200

    use link instead of Link to allow styling of links

--- src/components/navigation/MainNavigation.svelte
+++ src/components/navigation/MainNavigation.svelte
@@ -1,24 +1,21 @@
 <script>
-    import { Router, Route, Link } from "svelte-routing";
+    import { Router, Route, link } from "svelte-routing";
     import Pokedex from "../Pokedex.svelte";
     import News from "../News.svelte";
     import Team from "../Team.svelte";
     export let pokemon = [];
-    export let url = "";
 </script>
 <div>
-    <Router url="{url}">
-        <nav class="main-nav-flex-container">
-            <Link to="news">News</Link>
-            <Link to="/">Pokédex</Link>
-            <Link to="team">Manage Your Team</Link>
-        </nav>
-        <div>
+    <nav class="main-nav-flex-container">
+        <Router>
+            <a href='/news' class='main-nav-link' use:link>News</a>
+            <a href='/' class='main-nav-link' use:link>Pokédex</a>
+            <a href='/team' class='main-nav-link' use:link>Manage Your Team</a>
             <Route path="news"><News /></Route>
             <Route path="/"><Pokedex pokemon={pokemon}/></Route>
             <Route path="/team"><Team /></Route>
-        </div>
-    </Router>
+        </Router>
+    </nav>
 </div>
 
 <style>
@@ -28,5 +25,22 @@
         align-items: center;
         list-style: none;
         width: 100%;
+    }
+
+    .main-nav-link {
+        text-decoration: none;
+        margin-bottom: 16px;
+    }
+
+    @media screen and (max-width: 600px) {
+        .main-nav-flex-container {
+            display: flex;
+            justify-content: center;
+            align-items: start;
+            flex-direction: column;
+            list-style: none;
+            width: 90%;
+            margin-left: 20px;
+        }
     }
 </style>
